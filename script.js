9a5cdd56ce411e4bd90c4171995f93d8bd0e99a0
aktann615 <ambertann615@gmail.com>
2024-05-04 19:36:41 -0400

    Game Commit1-styling

---
+++ script.js
@@ -0,0 +1,25 @@
+// alert("Yooooo!")
+
+
+// ☐ Include win/loss logic and render win/loss messages in HTML. Popup alerts using the alert() method are okay during development, but not production.
+
+// ☐ Have properly indented HTML, CSS & JavaScript. In addition, vertical whitespace needs to be consistent.
+
+// ☐ No remaining unused and/or commented out code (code that will never be called) .
+
+// ☐ Have functions and variables that are named sensibly. Remember, functions are typically named as verbs and variables (data) named as nouns.
+
+// ☐ Be coded in a consistent manner. For example, choose between your preference for function declarations vs. function expressions.
+
+// ☐ Be deployed online using GitHub Pages so that the rest of the world can play your game!
+
+// An array of cards
+// function MATH.RANDOM()
+//FOR LOOPS
+
+// I don't HAVE to use for loops; i can use if else else statements as long as it works.
+
+// click button events
+// -for when the cards flip
+// -if there is already a match
+// -if there is no match (sort of reset)
