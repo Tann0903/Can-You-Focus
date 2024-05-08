9ae91ca6c9ccd2b78b3c0882c2505e9e835559e6
aktann615 <ambertann615@gmail.com>
2024-05-07 22:53:45 -0400

    C2-Styling and Logic

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 // alert("Yooooo!")
 
-
-// ☐ Include win/loss logic and render win/loss messages in HTML. Popup alerts using the alert() method are okay during development, but not production.
-
-// ☐ Have properly indented HTML, CSS & JavaScript. In addition, vertical whitespace needs to be consistent.
-
-// ☐ No remaining unused and/or commented out code (code that will never be called) .
-
-// ☐ Have functions and variables that are named sensibly. Remember, functions are typically named as verbs and variables (data) named as nouns.
-
-// ☐ Be coded in a consistent manner. For example, choose between your preference for function declarations vs. function expressions.
-
-// ☐ Be deployed online using GitHub Pages so that the rest of the world can play your game!
-
 // An array of cards
-// function MATH.RANDOM()
+// function MATH.RANDOM()?
 //FOR LOOPS
 
 // I don't HAVE to use for loops; i can use if else else statements as long as it works.
@@ -23,3 +10,51 @@
 // -for when the cards flip
 // -if there is already a match
 // -if there is no match (sort of reset)
+// -SOUNDS
+// - setTimeout events (card flips)
+
+// shuffling function
+// difference between 1st click (when user is guessing) and 2nd click (when checking a match)
+
+// POP UP MODULE TO DISPLAY THE RULES - yes
+// WIN LOSS POP UP - yes
+// LINK that Paul sent
+
+// ***************************************
+
+let gameCards = [
+    {value: "Ghost", image: "./img/Ghost-icon.png"},
+    {value: "Death", image: "./img/Death-icon.png"},
+    {value: "Ghost", image: "./img/Ghost-icon.png"},
+    {value: "Death", image: "./img/Death-icon.png"},
+]
+// The cards in this array are objects
+// values and image are properties
+
+document.querySelectorAll(".gameCards")
+//document. que... - Open the page, use inspect tools and paste it into the console. You should see the cards highlighted in the DOM.
+
+// ELEMENTS!!
+gameElements.array.forEach(element => {
+    gameElements.
+});
+
+
+// STACK OVERFLOW SHUFFLE
+function shuffle(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
