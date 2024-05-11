1d3312937dbd7d239d05909aebbe34b58c3b728d
aktann615 <ambertann615@gmail.com>
2024-05-11 18:19:19 -0400

    C3-Logic

--- script.js
+++ script.js
@@ -1,60 +1,69 @@
-// alert("Yooooo!")
+// options_unclicked = [1, 2, 3, 4]
+// clicked = []
 
-// An array of cards
-// function MATH.RANDOM()?
-//FOR LOOPS
+// // win logic
+// winning_combos = [
+//     [1,2],
+//     [3,4],
+// ]
 
-// I don't HAVE to use for loops; i can use if else else statements as long as it works.
+// 1 = document.querySelector('button1')
+// userClick()
+// clicked.push()
 
-// click button events
-// -for when the cards flip
-// -if there is already a match
-// -if there is no match (sort of reset)
-// -SOUNDS
-// - setTimeout events (card flips)
-
-// shuffling function
-// difference between 1st click (when user is guessing) and 2nd click (when checking a match)
-
-// POP UP MODULE TO DISPLAY THE RULES - yes
-// WIN LOSS POP UP - yes
-// LINK that Paul sent
+// // compare clicked to winning combos
+// if clicked === winning_combos[1]{
+//     console.log(win)
+// }
 
 // ***************************************
-
-let gameCards = [
-    {value: "Ghost", image: "./img/Ghost-icon.png"},
-    {value: "Death", image: "./img/Death-icon.png"},
-    {value: "Ghost", image: "./img/Ghost-icon.png"},
-    {value: "Death", image: "./img/Death-icon.png"},
-]
-// The cards in this array are objects
-// values and image are properties
-
-document.querySelectorAll(".gameCards")
-//document. que... - Open the page, use inspect tools and paste it into the console. You should see the cards highlighted in the DOM.
-
-// ELEMENTS!!
-gameElements.array.forEach(element => {
-    gameElements.
-});
-
-
 // STACK OVERFLOW SHUFFLE
 function shuffle(array) {
-    let currentIndex = array.length;
-  
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
+  let currentIndex = array.length;
 
-    return array;
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
+
+  return array;
+}
+
+let gameIcons = [
+    { value: "Ghost", image: "./img/Ghost-icon.png" },
+    { value: "Death", image: "./img/Death-icon.png" },
+    { value: "Ghost", image: "./img/Ghost-icon.png" },
+    { value: "Death", image: "./img/Death-icon.png" },
+  ];
+  
+  let clicked = []
+  
+  function userClick(icon) {
+      clicked.push(icon);
+  }
+  
+  // Shuffle gameIcons array
+  shuffle(gameIcons);
+  
+  // Winning Combos
+  let winningCombos = [["Ghost", "Ghost"], ["Death", "Death"]];
+  
+  let iconsEl = document.querySelectorAll(".gameIcons");
+  
+  iconsEl.forEach(function (el, index) {
+    el.addEventListener("click", function () {
+      // Get the clicked icon
+      let clickedIcon = gameIcons[index];
+      // Push the clicked icon to the clicked array
+      userClick(clickedIcon);
+      console.log("Clicked icons", clicked);
+    });
+  });
